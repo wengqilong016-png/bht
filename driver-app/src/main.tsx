7ba92ca7da05948fc86c6ab63d4df5fdb7786833
wowowo <wengqilong016@gmail.com>
2026-03-15 10:16:36 +0300

    Update to use StrictMode only in DEV

--- driver-app/src/main.tsx
+++ driver-app/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <>
+      {/* Use StrictMode only in development */}
+      {process.env.NODE_ENV === 'development' ? <React.StrictMode><MainComponent /></React.StrictMode> : <MainComponent />}
+    </>
+  );
+};
+
+export default App;
